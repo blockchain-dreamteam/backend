91c54e0f03fd9f81c2d283bfdce1da810e16abec
Ubuntu <ubuntu@ip-172-31-2-114.us-east-2.compute.internal>
2020-08-16 19:37:11 +0000

    ci: add backend setup based on fabcar

---
+++ credhistory/startFabric.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error
+set -e
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+starttime=$(date +%s)
+CC_SRC_LANGUAGE="typescript"
+CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
+CC_SRC_PATH="./chaincode/
+
+
+# clean out any old identites in the wallets
+rm -rf typescript/wallet/*
+
+# launch network; create channel and join peer to channel
+pushd ../network
+./network.sh down
+./network.sh up createChannel -ca -s couchdb
+./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
+popd
+
+cat <<EOF
+
+Total setup execution time : $(($(date +%s) - starttime)) secs ...
+
+Next, use the FabCar applications to interact with the deployed FabCar contract.
+The FabCar applications are available in multiple programming languages.
+Follow the instructions for the programming language of your choice:
+
+  Start by changing into the "blockchain-client" directory:
+    cd blockchain-client
+
+  Next, install all required packages:
+    npm install
+
+  Next, compile the TypeScript code into JavaScript:
+    npm run build
+
+  Then run the following applications to enroll the admin user, and register a new user
+  called appUser which will be used by the other applications to interact with the deployed
+  FabCar contract:
+    node dist/enrollAdmin
+    node dist/registerUser
+
+  You can run the invoke application as follows. By default, the invoke application will
+  create a new car, but you can update the application to submit other transactions:
+    node dist/invoke
+
+  You can run the query application as follows. By default, the query application will
+  return all cars, but you can update the application to evaluate other transactions:
+    node dist/query
+
+EOF
---
+++ setup/hyperledger_fabric_prerequisites_setup.sh
@@ -0,0 +1,28 @@
+#sudo apt-get install curl git docker.io docker-compose nodejs npm python
+
+# Updating npm to 5.6.0
+#sudo npm install npm@5.6.0 -g
+
+# Updating nodejs
+sudo npm install -g n
+n stable
+node --version
+
+# Setting up docker configuration
+#sudo usermod -a -G docker $USER
+
+#sudo systemctl start docker
+#sudo systemctl enable docker
+
+# Installing golang
+#wget https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
+#tar -xzvf go1.13.6.linux-amd64.tar.gz && rm "go1.13.6.linux-amd64.tar.gz"
+#sudo mv go/ /usr/local
+
+# edit gopath in .bashrc
+#echo '' >> ~/.bashrc
+#echo '# GOPATH configuration for hyperledger' >> ~/.bashrc
+#echo 'export GOPATH=/usr/local/go' >> ~/.bashrc
+#echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+
+#echo "Relogin"
