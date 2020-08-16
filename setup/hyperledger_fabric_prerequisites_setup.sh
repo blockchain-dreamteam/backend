3d723faa6cff76f9777344b304c9e7811e151d60
Ubuntu <ubuntu@ip-172-31-2-114.us-east-2.compute.internal>
2020-08-16 19:45:06 +0000

    fix: uncomment commands

--- setup/hyperledger_fabric_prerequisites_setup.sh
+++ setup/hyperledger_fabric_prerequisites_setup.sh
@@ -1,7 +1,7 @@
-#sudo apt-get install curl git docker.io docker-compose nodejs npm python
+sudo apt-get install curl git docker.io docker-compose nodejs npm python
 
 # Updating npm to 5.6.0
-#sudo npm install npm@5.6.0 -g
+sudo npm install npm@5.6.0 -g
 
 # Updating nodejs
 sudo npm install -g n
@@ -9,20 +9,20 @@ n stable
 node --version
 
 # Setting up docker configuration
-#sudo usermod -a -G docker $USER
+sudo usermod -a -G docker $USER
 
-#sudo systemctl start docker
-#sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Installing golang
-#wget https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
-#tar -xzvf go1.13.6.linux-amd64.tar.gz && rm "go1.13.6.linux-amd64.tar.gz"
-#sudo mv go/ /usr/local
+wget https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
+tar -xzvf go1.13.6.linux-amd64.tar.gz && rm "go1.13.6.linux-amd64.tar.gz"
+sudo mv go/ /usr/local
 
 # edit gopath in .bashrc
-#echo '' >> ~/.bashrc
-#echo '# GOPATH configuration for hyperledger' >> ~/.bashrc
-#echo 'export GOPATH=/usr/local/go' >> ~/.bashrc
-#echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+echo '' >> ~/.bashrc
+echo '# GOPATH configuration for hyperledger' >> ~/.bashrc
+echo 'export GOPATH=/usr/local/go' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 
-#echo "Relogin"
+echo "Relogin"
